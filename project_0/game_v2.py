e134a04c99928a4a149b479ca9dfaf31dbb58ed1
Maria Myasnikova <masha26778.ru>
2021-11-24 16:35:35 +0500

    edit readme.md

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,47 +1,22 @@
-"""Игра угадай число
-компьютер сам загадывает и угадывает число
-"""
+"""Игра угадай число"""
 
 import numpy as np
 
-def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+number = np.random.randint(1, 101) #загадываем число
+
+# количество попыток
+count = 0
+
+while True:
+    count+=1
+    predict_number = int(input("Угадай число от 1 до 100:"))
     
-    Args:
-        number (int, optional): Загадываем число. Defaults to 1.
+    if predict_number > number:
+        print("Число должно быть меньше!")
         
-    Returns:
-        int: Число попыток
-    """
-    count = 0
-
-    while True:
-        count+=1
-        predict_number = np.random.randint(1, 101) #Предполагаемое число
-        if number==predict_number:
-            break #Выход из цикла если угадали
-    return(count)
-
-
-def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
-
-    Args:
-        random_predict ([type]): функция угадывания
-
-    Returns:
-        int: среднее количество попыток
-    """
-    count_ls = []
-    np.random.seed(1) #фиксируем сид для воспроизводимости
-    random_array = np.random.randint (1, 101, size=(1000)) #загадали список чисел
-    
-    for number in random_array:
-        count_ls.append(random_predict(number))
+    elif predict_number < number:
+        print("Число должно быть больше!")
         
-        score = int(np.mean(count_ls))
-        print(f'Ваш алгоритм угадывает число в среднем за:{score} попыток')
-        return(score)
-    
-# RUN
-score_game(random_predict)
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break #конец игры выход из цикла
